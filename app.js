c0b7b8c70f4da567263a3b79d9eeaeac630e15fc
James Lawrence <jamesblawrence2007@gmail.com>
2025-01-07 09:38:55 -0700

    added form to add friend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const friends = [];
+const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -8,6 +9,20 @@ app.get('/getCurrentDate', (req, res) => {
     const date = new Date();
     res.json({'Current Date': date});
 });
+app.post('/addFriend'), (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const friend = {
+        name: name, 
+        age: age, 
+        email: email, 
+        phone: phone
+    };
+    friends.push(friend);
+    res.json(friends);
+};
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
