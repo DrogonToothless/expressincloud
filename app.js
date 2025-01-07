cb788de8da3e5de5c76fbc691eb761512aae8160
James Lawrence <jamesblawrence2007@gmail.com>
2025-01-07 08:08:22 -0700

    added index.html

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const app = express();
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.get('/getCurrentDate', (req, res) => {
+    const date = new Date();
+    res.json({'Current Date': date});
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
